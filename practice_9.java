0605311590cbc8718e37c0a7341151aee253731b
Minh.PV <minhpv956@gmail.com>
2026-05-06 10:38:03 +0700

    operator

---
+++ practice_9.java
@@ -0,0 +1,25 @@
+package session_2;
+
+public class practice_9 {
+
+    public static void main(String[] args) {
+
+        final int x = 10;
+
+        int x1 = x;
+        x1++;
+        System.out.println("X after plus 1:"+x1);
+
+        int x2 = x;
+        x2--;
+        System.out.println("x after minus 1:"+x2);
+
+        int x3 = x;
+        x3+=5;
+        System.out.println("x after plus 5 value:"+x3);
+
+
+
+    }
+
+}
